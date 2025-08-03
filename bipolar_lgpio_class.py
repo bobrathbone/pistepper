d38c555ee374daf035ff83c2743828588c5ab652
Bob Rathbone <bob@bobrathbone.com>
2025-08-03 13:21:34 +0100

    Version 2.3
    bipolar_class.py converted to bipolar_lgpio_class.py

--- bipolar_lgpio_class.py
+++ bipolar_lgpio_class.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 #
-# $Id: bipolar_class.py,v 1.7 2025/08/02 13:56:45 bob Exp $
+# $Id: bipolar_lgpio_class.py,v 1.5 2025/08/03 11:57:32 bob Exp $
 # Raspberry Pi bipolar Stepper Motor Driver Class
 # Hardware Nema17 12 Volt Stepper High Torque Motor
 # Gear Reduction Ratio: 1/64 
 # Uses the A4988 H-bridge circuit driver board.
+#
+# This version was converted from bipolar_class.py to use the Pyton3 lgpio library
+# See https://abyz.me.uk/lg/py_lgpio.html
 #
 # Author : Bob Rathbone
 # Site   : http://www.bobrathbone.com
@@ -14,12 +17,14 @@ import sys
 import os
 import time
 
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
+import lgpio
 
 # The stepper motor can be driven in five different modes 
 # See http://en.wikipedia.org/wiki/Stepper_motor
 
 # Step resolution (The last column is the multiplier for one revolution)
+# The first three parameters are the settings for ms1, ms2 and ms3 windings respectively 
 FullStep = [0,0,0,1]
 HalfStep = [1,0,0,2]
 QuarterStep = [0,1,0,4]
@@ -27,8 +32,8 @@ EighthStep = [1,1,0,8]
 SixteenthStep = [1,1,1,16]
 
 # Other definitions
-ENABLE = GPIO.LOW
-DISABLE = GPIO.HIGH
+HIGH = 1
+LOW = 0
 STEPS = 200 # 200 step motor (Full)
 
 class Motor:
@@ -58,53 +63,75 @@ class Motor:
 
     # Initialise GPIO pins for this unipolar motor
     def init(self):
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setwarnings(False)
-        GPIO.setup(self.direction,GPIO.OUT)
-        GPIO.setup(self.step,GPIO.OUT)
-        GPIO.setup(self.enable,GPIO.OUT)
-        GPIO.setup(self.ms1,GPIO.OUT)
-        GPIO.setup(self.ms2,GPIO.OUT)
-        GPIO.setup(self.ms3,GPIO.OUT)
+        self.chip = self.get_chip()
+        lgpio.gpio_claim_output(self.chip,self.direction,lgpio.SET_PULL_UP)
+        lgpio.gpio_claim_output(self.chip,self.step,lgpio.SET_PULL_UP)
+        lgpio.gpio_claim_output(self.chip,self.enable,lgpio.SET_PULL_UP)
+        lgpio.gpio_claim_output(self.chip,self.ms1,lgpio.SET_PULL_UP)
+        lgpio.gpio_claim_output(self.chip,self.ms2,lgpio.SET_PULL_UP)
+        lgpio.gpio_claim_output(self.chip,self.ms3,lgpio.SET_PULL_UP)
         self.zeroPosition()
         self.setStepSize(self.FULL)
         return  
 
+    # Open chip depending upon the Rasberry Pi model
+    def get_chip(self): 
+        # The Raspberry Pi Model 5 uses the RP1 chip (4). Try to open first
+        if os.path.exists("/proc/device-tree/aliases/gpio4"):
+            try:
+                chip = lgpio.gpiochip_open(4)
+                #print("Using RP1 chip",4,hex(chip))
+            except Exception as e:
+                print ("Fatal error: %s" % (str(e)))
+                sys.exit(1)
+        else:
+            # Earlier Raspberry Pi 4b,3B etc uses SOC chip  for I/O (0).
+            try:
+                chip = lgpio.gpiochip_open(0)
+                #print("Using chip",0,hex(chip))
+            except Exception as e:
+                print ("Fatal error: %s" % (str(e)))
+                sys.exit(1)
+        return chip
+
     # Reset (stop) motor
     def reset(self):
-        GPIO.output(self.step,GPIO.LOW)
-        GPIO.output(self.direction,GPIO.LOW)
-        GPIO.output(self.enable,GPIO.HIGH)
-        GPIO.output(self.ms1,GPIO.LOW)
-        GPIO.output(self.ms2,GPIO.LOW)
-        GPIO.output(self.ms3,GPIO.LOW)
+        lgpio.gpio_write(self.chip,self.step,LOW)
+        lgpio.gpio_write(self.chip,self.direction,LOW)
+        lgpio.gpio_write(self.chip,self.enable,LOW)
+        lgpio.gpio_write(self.chip,self.ms1,LOW)
+        lgpio.gpio_write(self.chip,self.ms2,LOW)
+        lgpio.gpio_write(self.chip,self.ms3,LOW)
         return  
 
     # Set up stepper resolution
     def setStepResolution(self,stepres):
-        GPIO.output(self.ms1,stepres[0])
-        GPIO.output(self.ms2,stepres[1])
-        GPIO.output(self.ms3,stepres[2])
+        lgpio.gpio_write(self.chip,self.ms1,stepres[0])
+        lgpio.gpio_write(self.chip,self.ms2,stepres[1])
+        lgpio.gpio_write(self.chip,self.ms3,stepres[2])
         self.oneRevolution = STEPS * stepres[3]
         return self.oneRevolution
 
     # Turn the motor
     def turn(self,steps,direction):
         count = steps
-        GPIO.output(self.enable,ENABLE)
-        GPIO.output(self.direction,direction)
+        lgpio.gpio_write(self.chip,self.enable,HIGH)
+        lgpio.gpio_write(self.chip,self.direction,direction)
         while count > 0:
-            GPIO.output(self.step,GPIO.HIGH)
+            lgpio.gpio_write(self.chip,self.step,HIGH)
             time.sleep(self.pulse)
-            GPIO.output(self.step,GPIO.LOW)
+            lgpio.gpio_write(self.chip,self.step,LOW)
             time.sleep(self.interval)
             count -= 1
-        GPIO.output(self.enable,DISABLE)
+        lgpio.gpio_write(self.chip,self.enable,LOW)
         return
 
     def interrupt(self):
         self.halt = True
         return
+
+    def close(self):
+        lgpio.gpiochip_close(self.chip)
 
     # Increment current position 
     def incrementPosition(self):
@@ -182,3 +209,49 @@ class Motor:
 
 # End of Unipolar Motor class
 
+# Test routine
+if __name__ == '__main__':
+    # 40 pin header for newer Raspberry Pi's
+    step = 21
+    direction = 20
+    enable = 25     # Not necessarily required (connect to control enable pin)
+    ms1 = 18
+    ms2 = 15
+    ms3 = 14
+    motora = Motor(step,direction,enable,ms1,ms2,ms3) 
+    print("direction",direction)
+    print("enable",enable)
+    print("ms1",ms1)
+    print("ms2",ms2)
+    print("ms3",ms3)
+    motora.init()
+
+    # Get the number of steps per revoltion
+    count = 3
+    while count > 0:
+        print ("Motor A Clockwise Full step")
+        revolution = motora.setStepSize(Motor.FULL)
+        motora.turn(revolution*3, Motor.CLOCKWISE)
+        count -= 1
+        time.sleep(1)
+
+    count = 3
+    while count > 0:
+        print ("Motor A Anticlockwise Full step")
+        revolution = motora.setStepSize(Motor.FULL)
+        motora.turn(revolution*1, Motor.ANTICLOCKWISE)
+        count -= 1
+        time.sleep(1)
+
+        print ("Motor A Clockwise Sixteenth step")
+        revolution = motora.setStepSize(Motor.SIXTEENTH)
+        motora.turn(revolution/2, Motor.CLOCKWISE)
+        time.sleep(1)
+
+    # Close the motor
+    motora.reset()
+    motora.close()
+# End of test program
+
+# set tabstop=4 shiftwidth=4 expandtab
+# retab
--- create_tar.sh
+++ create_tar.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# $Id: create_tar.sh,v 1.8 2025/08/01 06:35:31 bob Exp $
+# $Id: create_tar.sh,v 1.9 2025/08/03 12:18:02 bob Exp $
 
-FILELIST=" test_unipolar_class.py unipolar_class.py single_motor.py test_position.py motord.py motor_daemon.py log_class.py bipolar_class.py test_nema17.py test_26_nema17.py test_motor_i2c_class.py motor_i2c_class.py README RPi" 
+FILELIST=" test_unipolar_class.py unipolar_class.py single_motor.py test_position.py motord.py motor_daemon.py log_class.py bipolar_class.py bipolar_lgpio_class.py test_nema17.py test_26_nema17.py test_motor_i2c_class.py motor_i2c_class.py README README.md RPi" 
 
 # Create robot software tar file
 tar --exclude=CVS -cvzf  pi_stepper_motor.tar.gz ${FILELIST}
